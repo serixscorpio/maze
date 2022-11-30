379ba7312fa253f3f3d3d099113b14090c89d8d9
Eric Ti-Yu Chiang <serixscorpio@gmail.com>
2022-11-30 13:39:15 +0800

    Wire together cli example use of the package (#128)
    Implement a maze generation example that can be run via command line:
    
    ```sh
    poetry run maze
    ```
    
    The maze generation example produces a cat shaped maze (the mask file is treated as a source file, at `maze/src/cat.png`).
    
    Also some housekeeping documentation and dependency updates.

--- src/maze/__main__.py
+++ src/maze/__main__.py
@@ -1,11 +1,15 @@
 """Command-line interface."""
 import click
 
+from maze import example_gen_png
+
 
 @click.command()
 @click.version_option()
 def main() -> None:
-    """Maze."""
+    """Generate an example maze."""
+    with open("cat_maze.png", mode="wb") as f:
+        example_gen_png(f)
 
 
 if __name__ == "__main__":
